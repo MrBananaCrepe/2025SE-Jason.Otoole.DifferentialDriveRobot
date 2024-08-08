e00ddf20a6f5d009d075e05d77fb2888dd6e183e
Jason O <jason.otoole3@education.nsw.gov.au>
2024-08-09 09:20:57 +1000

    Update DifferentialDriveRobot_servos.ino

--- DifferentialDriveRobot/DifferentialDriveRobot_servos/DifferentialDriveRobot_servos.ino
+++ DifferentialDriveRobot/DifferentialDriveRobot_servos/DifferentialDriveRobot_servos.ino
@@ -1,59 +1,57 @@
 #include <Servo.h>
 #include <Arduino.h>
 
-#define SERVO_RIGHTPIN 12
-#define SERVO_LEFTPIN 11
-#define SENSOR_RIGHTPIN 10
-#define SENSOR_LEFTPIN 9
+#define SERVO_PINR 9
+#define SERVO_PINL 11
+int signalPinL = 12;
+int signalPinR = 13;
 
-Servo myServo;
+Servo myServoL;
+Servo myServoR;
 
-int pos = 0;
+void setup () {
+  Serial.begin(9600);
+  pinMode(signalPinL, INPUT);
+  pinMode(signalPinR, INPUT);
 
-class Servo
-{
-private:
-  byte pin;
-public:
-  Servo() {} // no use
-
-  Servo(byte pin)
-  {
-    this->pin = pin;
-  }
-
-  void init()
-  {
-    pinMode(pin, OUTPUT);
-  }
-
-  void init(byte defaultState)
-  {
-    init();
-    if (defaultState == 1) {
-      on();
-    }
-    else {
-      off();
-    }
-  }
-
-  void on()
-  {
-    digitalWrite(pin, 1);
-  }
-
-  void off()
-  {
-    digitalWrite(pin, 0);
-  }
-};
-
-void setup() {
-  myServo.attach(11);
-  myServo.write(90);
+  // 2300 = forward | 700 = backward | 1500 = stop
+  myServoL.attach(SERVO_PINR, 700, 2300);
+  myServoR.attach(SERVO_PINL, 2300, 700); // flipped bc servo reversed
 }
 
 void loop() {
-  myServo.write(180);
+  // drive forward
+  if(1 == digitalRead(signalPinL && signalPinR)) {
+    myServoL.writeMicroseconds(2300);
+    myServoR.writeMicroseconds(2300);
+    delay(100);
+  }
+
+  // turn left
+  else if(1 == digitalRead(signalPinL) && 0 == digitalRead(signalPinR)) {
+    myServoL.writeMicroseconds(2300);
+    myServoR.writeMicroseconds(1500);
+    delay(100);
+  }
+
+  // turn right
+  else if(0 == digitalRead(signalPinL) && 1 == digitalRead(signalPinR)) {
+    myServoL.writeMicroseconds(1500);
+    myServoR.writeMicroseconds(2300);
+    delay(100);
+  }
+
+  // stop
+  else if(0 == digitalRead(signalPinL) && 0 == digitalRead(signalPinR)) {
+    myServoL.writeMicroseconds(1500);
+    myServoR.writeMicroseconds(1500);
+    delay(100);
+  }
 }
+
+
+
+
+
+
+
